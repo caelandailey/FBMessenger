79c1aa88068d1706cbaae6c8c9fe7cf45bda82e2
caelandailey <caelancsd@hotmail.com>
2017-07-29 12:28:36 -0500

    initial commit
    Contains UI elements for message list

---
+++ FBMessenger/FriendsController.swift
@@ -0,0 +1,234 @@
+//
+//  ViewController.swift
+//  FBMessenger
+//
+//  Created by Caelan Dailey on 7/24/17.
+//  Copyright © 2017 Caelan Dailey. All rights reserved.
+//
+
+import UIKit
+
+class Friend: NSObject {
+    
+    var name: String?
+    var profileImageName: String?
+}
+
+class Message: NSObject {
+    
+    var text: String?
+    var date: NSDate?
+    
+    var friend: Friend?
+}
+
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    private let cellId = "cellId"
+    
+    var messages: [Message]?
+    
+    func setupData() {
+        
+        let mark = Friend()
+        mark.name = "Mark Zuckerberg"
+        mark.profileImageName = "zuckprofile"
+        
+        let message = Message()
+        message.friend = mark
+        message.text = "Hey, nice to meet you!"
+        message.date = NSDate()
+        
+        messages = [message]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Recent"
+        
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.register(MessageCell.self, forCellWithReuseIdentifier: cellId)
+        
+        setupData()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let count = messages?.count {
+            return count
+        }
+        return 0
+    }
+
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MessageCell
+        
+        if let message = messages?[indexPath.item] {
+            cell.message = message
+        }
+            
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width,height: 100)
+    }
+}
+
+class MessageCell: BaseCell {
+    
+    var message: Message? {
+        didSet {
+            nameLabel.text = message?.friend?.name
+            if let profileImageName = message?.friend?.profileImageName {
+                profileImageView.image = UIImage(named: profileImageName)
+            }
+            
+            messageLabel.text = message?.text
+            
+            if let date = message?.date {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "h:mm a"
+                
+                timeLabel.text = dateFormatter.string(from: date as Date)
+            }
+        }
+    }
+    
+    let profileImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.layer.cornerRadius = 34
+    imageView.layer.masksToBounds = true
+    return imageView
+    }()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mark Zuckerberg"
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your friend's message and something else..."
+        label.textColor = UIColor.darkGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12:05 pm"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .right
+        return label
+    }()
+    
+    let hasReadImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    override func setupViews() {
+        
+        addSubview(profileImageView)
+        addSubview(dividerLineView)
+        
+        setupContainerView()
+        
+        profileImageView.image = UIImage(named: "zuckprofile")
+        hasReadImageView.image = UIImage(named: "zuckprofile")
+        
+        addConstraintsWithFormat("H:|-12-[v0(68)]", views: profileImageView)
+        addConstraintsWithFormat("V:[v0(68)]", views: profileImageView)
+        
+        addConstraint(NSLayoutConstraint(item: profileImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        addConstraintsWithFormat("H:|-82-[v0]|", views: dividerLineView)
+        addConstraintsWithFormat("V:[v0(1)]|", views: dividerLineView)
+    }
+    
+    private func setupContainerView() {
+        let containerView = UIView()
+        addSubview(containerView)
+        
+        addConstraintsWithFormat("H:|-90-[v0]|", views: containerView)
+        addConstraintsWithFormat("V:[v0(50)]", views: containerView)
+        addConstraint(NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(hasReadImageView)
+        
+        containerView.addConstraintsWithFormat("H:|[v0][v1(80)]-12-|", views: nameLabel, timeLabel)
+        
+        containerView.addConstraintsWithFormat("V:|[v0][v1(24)]|", views: nameLabel, messageLabel)
+        
+        containerView.addConstraintsWithFormat("H:|[v0]-8-[v1(20)]-12-|", views: messageLabel, hasReadImageView)
+        
+        containerView.addConstraintsWithFormat("V:|[v0(24)]", views: timeLabel)
+        
+        containerView.addConstraintsWithFormat("V:[v0(20)]|", views: hasReadImageView)
+    }
+}
+
+extension UIView {
+    
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+    
+}
+
+class BaseCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecorder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        
+        backgroundColor = UIColor.blue
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
